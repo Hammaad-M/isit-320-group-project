d698e355a53375019f9d0ec9fe8f710e7f2ea9df
Hammaad <hammaad.memon@gmail.com>
2022-11-20 17:33:29 -0800

    collected code for 'individual github repo' submission

---
+++ firebaseconfig.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdwtAPwaM9mqHMXSrobtiWRGjLUidz6as",
+  authDomain: "align-d4038.firebaseapp.com",
+  projectId: "align-d4038",
+  storageBucket: "align-d4038.appspot.com",
+  messagingSenderId: "540706810436",
+  appId: "1:540706810436:web:b05f6cdd7736a5405b2a11",
+  measurementId: "G-NS4V9CKKD3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { app, analytics, auth, provider };
